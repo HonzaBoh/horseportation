52899cd1a3ceac586d2b33972e83d93296fd8ed1
jBohac <jbohac@students.zcu.cz>
2021-10-19 10:57:59 +0200

    Refreshing DataLoader.java, pushing datasets

---
+++ src/simulation/DataLoader.java
@@ -0,0 +1,10 @@
+package simulation;
+
+/**
+ * Trida pro nacitani vstupnich dat
+ * Data nacita dle parametru urcenych v zadani
+ * Slouzi k predani informaci: pocet + mnozina Koni, pocet + mnozina Letadel, souradnice Parize
+ * Veskere metody jsou staticke a slouzi k uhlednejsi komunikaci programu se zdrojem souboru za pouziti Scanneru
+ */
+public class DataLoader {
+}
