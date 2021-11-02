7127789ac7c0fe01bb5cee3fd7726fb0949888c3
jBohac <jbohac@students.zcu.cz>
2021-11-02 11:10:33 +0100

    Pripraven setup pred prvnim odevzdanim

--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -7,6 +7,10 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Startovaci bod programu
+     * TODO: vymyslet prvni pruchod simulaci, zajistit vypis zmeny stavu v case
+     */
     public static void main(String[] args) throws IOException {
         //DataLoader.descriptiveRead("Data\\parser.txt");
         List<Plane> tmpPlanes = new ArrayList<>();
--- src/simulation/SimulationEnvironment.java
+++ src/simulation/SimulationEnvironment.java
@@ -1,6 +1,8 @@
 package simulation;
 
-/***
+import java.util.List;
+
+/**
  * Background prostredi pro vsechny informace v prubehu simulace
  * Uskladni veskere objekty, ktere v simulaci figuruji
  * Uklada si souradnice Parize, ktere jsou vzdy jedenkrat za simulaci a jsou nemenne (napsana stylem 'final' bez final)
@@ -15,7 +17,23 @@ public class SimulationEnvironment {
      */
     public static double PARIS_Y;
 
+    /**
+     * Vypise soucasny stav vsech letadel pri jakekoliv zmene stavu
+     * Zmeny stavu: zacatek simulace, odlet, nakladani kone (zacatek i nalozeni), pristani, pristani Pariz
+     * @param planes jednotlive entity, jejich cas se vypisuje
+     * @param stops debugging ucel, sleduje vyznamne souradnice ve vztahu k vypisu
+     */
+    public void getStatus(List<Plane> planes, List<Town> stops){
 
+    }
+
+    /**
+     * Spousteci bod pro simulaci
+     * TODO: vytvoreni tridy mapa, predani jedne instance do simulace
+     */
+    public void runSimulation(){
+
+    }
 
 
 }
