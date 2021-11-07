b4336cfc25c921881ae2263cb7a0b7536d4b2d95
ester <ester.halirova@gmail.com>
2021-11-07 14:36:39 +0100

    First fly of all planes to Paris across some cities until full capacity of those planes

--- src/simulation/Mapa.java
+++ src/simulation/Mapa.java
@@ -3,39 +3,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Mapa { //nejde pojmenovat Map? Wtf :D
-
+    /**
+     * list letadel, která máme k dispozici
+     */
     List<Plane> planes = new ArrayList<>();
+    /**
+     * list mest, ktera musime proletet
+     */
     List<Town> towns = new ArrayList<>();
+    /**
+     * list mest, ktera jsme proleteli
+     */
     List<Town> townsDone = new ArrayList<>();
 
-    int p = 0; //number of plane
-
+    /**
+     * Konstruktor pro mapu
+     * @param towns list mest, ktera se musi proletet
+     * @param planes list letadel, ktera mame k dispozici
+     */
     public Mapa(List<Town> towns, List<Plane> planes) {
         this.planes = planes;
         this.towns = towns;
     }
 
-    /*
-    Metoda zatim! prvnimu letadlu z listu pritadi mesta tak, aby se mu neprekrocila kapacita, ukaze se tak prvni let
+    /**
+     *  Metoda naplni vsechna letadla co nejvice lze a vykona prvni let smerem do Parize
      */
     public void firstFly() {
-        System.out.println(planes.get(p).capacity);
-        System.out.println();
 
-        for (int i = 0; i < towns.size(); i++) {
-            if (planes.get(p).actualCapacity - towns.get(i).weight >= 0) {
-                planes.get(p).actualCapacity -= towns.get(i).weight;
-                townsDone.add(towns.get(i));
+        for (int p = 0; p < planes.size() && towns.size() != 0; p++) {
+            System.out.println("\nLetadlo: " + p + ", capacity = " + planes.get(p).capacity);
+            for (int i = 0; i < towns.size(); i++) {
+                if (planes.get(p).actualCapacity - towns.get(i).weight >= 0) {
+                    planes.get(p).actualCapacity -= towns.get(i).weight;
+                    System.out.println("\tLoading horses with weight: " + towns.get(i).weight);
+                    townsDone.add(towns.get(i));
+                    towns.remove(i);
+                    i = -1;
+                }
             }
+            System.out.println("Plane " + p + " is going to Paris with actual Capacity: " + planes.get(p).actualCapacity);
         }
-        for (int i = 0; i < townsDone.size(); i++) {
-            System.out.println(townsDone.get(i).weight);
-        }
-        System.out.println();
-        System.out.println(planes.get(p).actualCapacity);
-
-        /*
-        Zatim najde letadlu mesta ktera muze projet pri prvni ceste
-         */
     }
 }
